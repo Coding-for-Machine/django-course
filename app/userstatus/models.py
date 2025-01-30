3d21175a4e6a697e4c0581246950d5549acb7fb1
CodingforMachine <codingformachine@gmail.com>
2025-01-30 11:08:41 +0500

    my commit

--- app/userstatus/models.py
+++ app/userstatus/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from lessons.models import Lesson
+from users.models import MyUser
 # Create your models here.
+
+class UserLessonStatus(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    
