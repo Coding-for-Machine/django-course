3b182905f6040a225de31045a1b3bde6310abc99
CodingforMachine <codingformachine@gmail.com>
2025-02-11 16:45:29 +0500

    my commit secret.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,30 +4,30 @@
 if [ "$DJANGO_ENV" = "production" ]; then
     echo "Ishlab chiqarish muhitiga o'tilmoqda..."
     # Agar ishlab chiqarish bo'lsa, collectstatic ishlatilsin
-    python ./manage.py collectstatic --noinput
+    python manage.py collectstatic --noinput
 else
     echo "Ishlab chiqish muhitida..."
 fi
 
 # Migratsiyalarni bajarish
 echo "Migratsiyalarni bajarish..."
-python ./manage.py migrate
+python manage.py migrate
 
 # Superuser yaratish (agar mavjud bo'lmasa)
 echo "Superuser yaratish tekshirilmoqda..."
-python ./manage.py createsuperuser --noinput || echo "Superuser allaqachon mavjud"
+python manage.py createsuperuser --noinput || echo "Superuser allaqachon mavjud"
 
 # Keshni tozalash
 echo "Keshni tozalash..."
-python ./manage.py clear_cache
+python manage.py clear_cache
 
 # Log faylini yaratish va yangilash
-LOG_FILE="./logs/django.log"
+LOG_FILE="logs/django.log"
 echo "Django serveri boshlanishi..." > $LOG_FILE
 echo "Migratsiyalarni bajarish..." >> $LOG_FILE
 python manage.py migrate >> $LOG_FILE 2>&1
 echo "Superuser yaratish..." >> $LOG_FILE
-python ./manage.py createsuperuser --noinput >> $LOG_FILE 2>&1 || echo "Superuser allaqachon mavjud" >> $LOG_FILE
+python manage.py createsuperuser --noinput >> $LOG_FILE 2>&1 || echo "Superuser allaqachon mavjud" >> $LOG_FILE
 echo "Keshni tozalash..." >> $LOG_FILE
 python manage.py clear_cache >> $LOG_FILE 2>&1
 
