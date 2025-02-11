288ef3379c8847eefc63b69f4817bf64174ed450
CodingforMachine <codingformachine@gmail.com>
2025-02-11 16:39:45 +0500

    my commit secret.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -22,7 +22,7 @@ echo "Keshni tozalash..."
 python ./app/manage.py clear_cache
 
 # Log faylini yaratish va yangilash
-LOG_FILE="./app/app/logs/django.log"
+LOG_FILE="./app/logs/django.log"
 echo "Django serveri boshlanishi..." > $LOG_FILE
 echo "Migratsiyalarni bajarish..." >> $LOG_FILE
 python ./app/manage.py migrate >> $LOG_FILE 2>&1
@@ -34,7 +34,7 @@ python manage.py clear_cache >> $LOG_FILE 2>&1
 # Gunicorn serverini ishga tushurish (production uchun)
 if [ "$DJANGO_ENV" = "production" ]; then
     echo "Gunicorn serverini ishga tushurish..."
-    exec gunicorn app.wsgi:application --bind 0.0.0.0:8000 >> $LOG_FILE 2>&1
+    exec gunicorn app.wsgi:application --bind 0.0.0.0:8000 
 else
     # Django serverini ishga tushurish (staging uchun)
     echo "Django serveri ishga tushurilmoqda..."
